cb097df286415a1affd3a36bb928ef923c0f5b5b
WildBoarGonnaGo <unknonwthrill814@gmail.com>
2021-12-09 01:11:31 +0300

    week_3 interview progress

--- algs1/week_3/Interview/src/SmallerAuxiliaryArray.java
+++ algs1/week_3/Interview/src/SmallerAuxiliaryArray.java
@@ -4,17 +4,46 @@ a[n] to a[2*n−1] is sorted. How can you merge the two subarrays so that  a[0] 
 a[2*n−1] is sorted using an auxiliary array of length  n (instead of  2n)
 * */
 
-
 public class SmallerAuxiliaryArray {
-	public static void merge(Comparable[] target) {
+	private static void	merge(Comparable[] target) {
 		int				auxLength = target.length / 2;
 		int				arrlen = target.length;
 		Comparable[]	aux = new Comparable[auxLength];
 		for (int i = 0; i < auxLength; ++i)
 			aux[i] = target[i];
-		int start , mid = auxLength;
-		for (int i = 0; i < arrlen; ++i) {
-			if (start > )
+		int start = 0, mid = auxLength;
+		for (int i = 0; i < auxLength; ++i) {
+				if (aux[start].compareTo(target[mid]) <= 0) target[i] = aux[start++];
+				else {
+					Comparable buffer = aux[start];
+					aux[(start == 0) ? start : --start] = target[mid];
+					target[mid++] = buffer;
+				}
+			}
 		}
+	private class IntegerCompare implements Comparable<Integer> {
+		Integer	lval;
+
+		public IntegerCompare() { lval = 0; }
+		public IntegerCompare(Integer lval) { this.lval = lval; }
+
+		@Override
+		public int	compareTo(Integer rval) {
+			return lval - rval;
+		}
+
+		int		getLVal() { return lval; }
+		void	setLVal(Integer lval) { this.lval = lval; }
+	}
+
+	public static void main(String[] args) {
+		Comparable[]	arr = new Comparable[10];
+		for (int i = 5; i < 10; ++i) arr[i] = Integer.valueOf(i - 5);
+		for (int i = 0; i < 5; ++i) arr[i] = Integer.valueOf(i + 5);
+		System.out.println("Array before mergesort:");
+		for (int i = 0; i < 10; ++i) System.out.print(arr[i].toString() + ((i == 9) ? '\n' : ' '));
+		merge(arr);
+		System.out.println("Array after mergesort:");
+		for (int i = 0; i < 10; ++i) System.out.print(arr[i].toString() + ((i == 9) ? '\n' : ' '));
 	}
 }
