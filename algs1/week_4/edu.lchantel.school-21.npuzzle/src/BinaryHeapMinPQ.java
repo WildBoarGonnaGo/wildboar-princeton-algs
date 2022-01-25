34e30a7e85ce0a301d8a7c20d955abf1144bcb9c
WildBoarGonnaGo <unknonwthrill814@gmail.com>
2022-01-25 06:11:41 +0300

    There will be better priority queue with lgN search and insert complexity

---
+++ algs1/week_4/edu.lchantel.school-21.npuzzle/src/BinaryHeapMinPQ.java
@@ -0,0 +1,27 @@
+public class BinaryHeapMinPQ <Key extends Comparable<Key> >{
+	private Key[]	arr;
+	private int		capacity;
+	private int		size;
+
+	BinaryHeapMinPQ(int capacity) {
+		this.capacity = capacity;
+		size = 0;
+		arr = (Key[]) new Comparable[this.capacity];
+	}
+
+	BinaryHeapMinPQ() { this(1); }
+
+	public boolean	isEmpty() { return size == 0; }
+
+	private boolean	less(int i, int j) {
+		return arr[i].compareTo(arr[j]) < 0;
+	}
+
+	private	void	exch(int i, int j) {
+		Key	buffer = arr[i];
+		arr[i] = arr[j];
+		arr[j] = buffer;
+	}
+
+
+}
