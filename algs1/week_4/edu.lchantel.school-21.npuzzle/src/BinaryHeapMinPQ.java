70aae2c4b3cbc871504db8d8d6cbd40bc125452c
WildBoarGonnaGo <unknonwthrill814@gmail.com>
2022-01-26 06:24:44 +0300

    There will be better priority queue with lgN search and insert complexity, part 2

--- algs1/week_4/edu.lchantel.school-21.npuzzle/src/BinaryHeapMinPQ.java
+++ algs1/week_4/edu.lchantel.school-21.npuzzle/src/BinaryHeapMinPQ.java
@@ -23,5 +23,13 @@ public class BinaryHeapMinPQ <Key extends Comparable<Key> >{
 		arr[j] = buffer;
 	}
 
-
+	/**
+	 * Proposition is - the smallest key in a[1]
+	 * */
+	private void	swim(int k) {
+		while(k > 1 && less(k, k / 2)) {
+			exch(k, k/2);
+			k = k / 2;
+		}
+	}
 }
