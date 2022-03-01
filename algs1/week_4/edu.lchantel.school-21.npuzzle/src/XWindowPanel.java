7b8dc1075c03f06f740131acb852fd13330d9ad1
WildBoarGonnaGo <rat.machining.corp@yandex.ru>
2022-03-01 16:42:23 +0300

    xwindow_interface

--- algs1/week_4/edu.lchantel.school-21.npuzzle/src/XWindowPanel.java
+++ algs1/week_4/edu.lchantel.school-21.npuzzle/src/XWindowPanel.java
@@ -11,31 +11,41 @@ public class XWindowPanel extends JPanel implements ActionListener {
 	private final Image 			stoneFrame;
 	private final AffineTransform	transform;
 	private final Timer				timer;
+	private int						xV, yV, xCur, xEnd, yCur, yEnd;
 
 	public XWindowPanel () {
-		stoneFrame = new ImageIcon("/Users/wildboargonnago/wildboar-princeton-algs/algs1/week_4/edu.lchantel.school-21.npuzzle/resources/sandstone_frame.png")
+		stoneFrame = new ImageIcon("/home/zavelskiymm/wildboar-princeton-algs/algs1/week_4/edu.lchantel.school-21.npuzzle/resources/sandstone_frame.png")
 			.getImage();
 		transform = new AffineTransform();
-		transform.setTransform(0.1, 0, 0, 0.1, 100, 100);
-		timer = new Timer(1000, this);
+		transform.setTransform(0.2, 0, 0, 0.2, 100, 100);
+		timer = new Timer(500, this);
+		xV = 4;
+		xCur = 100; xEnd = 200;
+		yCur = 100; yEnd = 100;
 
 		setPreferredSize(new Dimension(600, 600));
+		timer.start();
 	}
 
 	@Override
 	public void	paint(Graphics g) {
-
+		super.paint(g);
 		Graphics2D g2D = (Graphics2D) g;
 		g2D.drawImage(stoneFrame, transform, null);
 
-		g2D.setFont(new Font("DooM", Font.PLAIN, 30));
+		g2D.setFont(new Font("DooM", Font.PLAIN, 60));
 		g2D.setColor(new Color(0xd21404));
-		g2D.drawString("5", 138, 162);
+		g2D.drawString("5", xCur + 76, yCur + 124);
 	}
 
 	@Override
 	public void			actionPerformed(ActionEvent e) {
-
+		xCur += xV;
+		if (xCur >= xEnd) timer.stop();
+		else if (xEnd - xCur <= 13) timer.setDelay(750);
+		else if (xEnd - xCur <= 25) timer.setDelay(600);
+		transform.setTransform(0.2, 0, 0, 0.2, xCur, yCur);
+		repaint();
 	}
 
 	public static void	main(String[] args) {
--- algs1/week_4/edu.lchantel.school-21.npuzzle/src/XWindowPrinter.java
+++ algs1/week_4/edu.lchantel.school-21.npuzzle/src/XWindowPrinter.java
@@ -1,10 +1,26 @@
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import java.awt.*;
+import java.util.LinkedList;
+import java.lang.IllegalArgumentException;
+import java.util.Iterator;
 
 public class XWindowPrinter extends JFrame {
-    public XWindowPrinter() {
+    private class Coord {
+        public int x;
+        public int y;
+    };
+    private LinkedList<Coord>    list;
 
+    public XWindowPrinter(NPuzzle puzzle) {
+        if (puzzle == null) throw new IllegalArgumentException();
+        list = new LinkedList<>();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+
+        }
     }
 
     public static void main(String[] args)
