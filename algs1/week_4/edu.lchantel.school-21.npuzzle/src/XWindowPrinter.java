9eb2325bbf0532b89eb1d7ee5b0a7ed970d35add
WildBoarGonnaGo <rat.machining.corp@yandex.ru>
2022-03-04 16:50:07 +0300

    xwindow_progress

--- algs1/week_4/edu.lchantel.school-21.npuzzle/src/XWindowPrinter.java
+++ algs1/week_4/edu.lchantel.school-21.npuzzle/src/XWindowPrinter.java
@@ -4,27 +4,41 @@ import java.awt.*;
 import java.util.LinkedList;
 import java.lang.IllegalArgumentException;
 import java.util.Iterator;
+import java.util.Arrays;
 
 public class XWindowPrinter extends JFrame {
     private class Coord {
-        public int x;
-        public int y;
+        private int x;
+        private int y;
+
+        public Coord(int x, int y) { this.x = x; this.y = y; }
+        public int getX() { return x; }
+        public int getY() { return y; }
     };
-    private LinkedList<Coord>    list;
+    private LinkedList<Coord>       list;
+    private LinkedList<BoardField>  boardList;
 
     public XWindowPrinter(NPuzzle puzzle) {
         if (puzzle == null) throw new IllegalArgumentException();
         list = new LinkedList<>();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Iterator it = list.iterator();
-        while (it.hasNext()) {
-
+        boardList = puzzle.solveList();
+        for (int i = 0; i < boardList.size(); ++i) {
+            int[][] tmp = boardList.get(i).getTiles();
+            int     tmpSize = boardList.get(i).size();
+            for (int j = 0; j < tmpSize; ++j) {
+                int found = Arrays.binarySearch(tmp[j], 0);
+                if (found >= 0) {
+                    list.addLast(new Coord(j, found));
+                    break ;
+                }
+            }
         }
+
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
     }
 }
